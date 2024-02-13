6161b4d1e040fbafc97cdf6ea2c1f7d1932f08cf
Arpita Dutta <arpitadutta808@gmail.com>
2024-02-13 17:14:51 +0530

    dialog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import OpensimpleDialog from "./components/OpensimpleDialog";
+import OpenalertDialog from "./components/OpenalertDialog";
+import SlidealertDialog from "./components/SlidealertDialog";
+import OpenformDialog from "./components/OpenformDialog";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <OpensimpleDialog/>
+      <OpenalertDialog/> <br/>
+      <SlidealertDialog/><br/>
+      <OpenformDialog/>
     </div>
   );
 }
